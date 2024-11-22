104a7e020f2a04e2a68f0a82d82f05fcaa49c3cd
Choi Minjune <minjune0807@gmail.com>
2024-11-23 00:10:36 +0900

    feat: Request 엔티티 연관관계 설정(Doctor, patient)

--- src/main/java/com/example/metapor/Domain/event/Request.java
+++ src/main/java/com/example/metapor/Domain/event/Request.java
@@ -1,9 +1,14 @@
 package com.example.metapor.Domain.event;
 
+import com.example.metapor.Domain.user.entity.Doctor;
+import com.example.metapor.Domain.user.entity.Patient;
+
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter// 모든 필드에 대해 getter 메소드를 자동으로 생성
@@ -14,8 +19,7 @@ import java.time.LocalDateTime;
 public class Request {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String applicantName; //신청자 이름
-
+    private Long id;
     @Column(name = "number_of_applicants", nullable = false)
     private Integer numberOfApplicants; //신청 인원
 
@@ -27,4 +31,12 @@ public class Request {
 
     @Column(name = "application_item", nullable = false)
     private String applicationItem; // 신청 항목
+
+    //Patients 연결
+    @OneToMany(mappedBy = "request", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Patient> patients = new ArrayList<>();
+
+    //Doctor 연결
+    @OneToMany(mappedBy = "request", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Doctor> doctors = new ArrayList<>();
 }
