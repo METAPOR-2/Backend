491476581f632ad9cd5c4bc13f898c80228eddd1
Choi Minjune <minjune0807@gmail.com>
2024-11-22 20:37:25 +0900

    init: 스프링 초기세팅

---
+++ src/main/java/com/example/metapor/MetaporApplication.java
@@ -0,0 +1,13 @@
+package com.example.metapor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaporApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetaporApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/metapor/MetaporApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.metapor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaporApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
